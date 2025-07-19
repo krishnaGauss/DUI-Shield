5d4ba02518e572a230a3f6740e86c24a9f77d7c6
krishnaGauss <goswamikrishna549@gmail.com>
2025-07-19 22:14:21 +0530

    adding initial logging file code

--- utils/logger.py
+++ utils/logger.py
@@ -7,3 +7,23 @@ def setup_logger():
     logger = logging.getLogger("DUIshield")
     logger.setLevel(LOG_LEVEL)
     
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler(LOG_FILE)
+    
+    c_handler.setLevel(LOG_LEVEL)
+    f_handler.setLevel(logging.DEBUG)  # to log all messages to file
+    
+    #creating formatters
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(formatter)
+    f_handler.setFormatter(formatter)
+    
+    if not logger.hasHandlers():
+        logger.addHandler(c_handler)
+        logger.addHandler(f_handler)
+        
+    return logger
+
+
+logger = setup_logger()
+    
